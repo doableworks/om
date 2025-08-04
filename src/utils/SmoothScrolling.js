279842dbff356088a0d97cedb4f838a60f5f1695
Mohitjnn <jnmohit29@gmail.com>
2025-08-05 04:20:52 +0530

    console.log removal 2

--- src/utils/SmoothScrolling.js
+++ src/utils/SmoothScrolling.js
@@ -3,8 +3,6 @@ import Lenis from "@studio-freight/lenis";
 /** Function for smooth scrolling of the website */
 export default function SmoothScrolling() {
 	const lenis = new Lenis();
-	lenis.on("scroll", () => {
-	});
 	/** Function For Smooth Scroll */
 	function raf(time) {
 		lenis.raf(time);
